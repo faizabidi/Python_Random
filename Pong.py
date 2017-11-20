37d9d4825270bec36d033b2a35b32b3f552dac51
faizabidi <fabidi89@vt.edu>
2017-11-20 11:06:28 -0500

    Minior chages.

--- Pong.py
+++ Pong.py
@@ -1,4 +1,6 @@
-#implementation of classic arcade game Pong
+# REQUIREMENT - Pygame - https://pypi.python.org/pypi/SimpleGUICS2Pygame
+
+# Implementation of classic arcade game Pong
 try:
     import simplegui
 except ImportError:
@@ -6,7 +8,7 @@ except ImportError:
 
 import random
 
-# initialize globals - pos and vel encode vertical info for paddles
+# Initialize globals - pos and vel encode vertical info for paddles
 WIDTH = 600
 HEIGHT = 400       
 BALL_RADIUS = 20
@@ -17,8 +19,8 @@ HALF_PAD_HEIGHT = PAD_HEIGHT / 2
 #LEFT = False
 #RIGHT = True
 
-# initialize ball_pos and ball_vel for new ball in middle of table
-# if direction is RIGHT, the ball's velocity is upper right, else upper left
+# Initialize ball_pos and ball_vel for new ball in middle of table
+# If direction is RIGHT, the ball's velocity is upper right, else upper left
 def spawn_ball(direction):
     global ball_pos, ball_vel 
     ball_pos = [WIDTH / 2, HEIGHT / 2]
@@ -29,7 +31,7 @@ def spawn_ball(direction):
     elif direction == "RIGHT":
         ball_vel = [ball_vel[0], -ball_vel[1]]
 
-# define event handlers
+# Define event handlers
 def new_game():
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
     global score1, score2  # these are ints
@@ -45,12 +47,12 @@ def new_game():
 def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
         
-    # draw mid line and gutters
+    # Draw mid line and gutters
     canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
     canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
     canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
         
-    # update ball
+    # Update ball
     ball_pos[0] += ball_vel[0]
     ball_pos[1] += ball_vel[1]
     
@@ -59,16 +61,16 @@ def draw(canvas):
     elif ball_pos[1] >= HEIGHT - BALL_RADIUS:
         ball_vel[1] = - ball_vel[1]
             
-    # draw ball
+    # Draw ball
     canvas.draw_circle(ball_pos, BALL_RADIUS, 1, "White", "White")
     
-    # update paddle's vertical position, keep paddle on the screen
+    # Update paddle's vertical position, keep paddle on the screen
     if paddle1_pos + paddle1_vel >= HALF_PAD_HEIGHT and paddle1_pos + paddle1_vel <= HEIGHT - HALF_PAD_HEIGHT:
         paddle1_pos += paddle1_vel
     if paddle2_pos + paddle2_vel >= HALF_PAD_HEIGHT and paddle2_pos + paddle2_vel <= HEIGHT - HALF_PAD_HEIGHT:
         paddle2_pos += paddle2_vel
     
-    # draw paddles
+    # Draw paddles
     canvas.draw_polygon([[0, paddle1_pos - HALF_PAD_HEIGHT],
                          [0, paddle1_pos + HALF_PAD_HEIGHT],
                          [PAD_WIDTH, paddle1_pos + HALF_PAD_HEIGHT],
@@ -81,7 +83,7 @@ def draw(canvas):
                          [WIDTH, paddle2_pos - HALF_PAD_HEIGHT]],
                         1, 'Red', 'Red')
     
-    # determine whether paddle and ball collide
+    # Determine whether paddle and ball collide
     if (ball_pos[0] <= PAD_WIDTH + BALL_RADIUS):
         if ball_pos[1] > paddle1_pos - HALF_PAD_HEIGHT and ball_pos[1] < paddle1_pos + HALF_PAD_HEIGHT:
             ball_vel[0] = - 1.1 * ball_vel[0]
@@ -95,7 +97,7 @@ def draw(canvas):
             score1 += 1
             spawn_ball("LEFT")
     
-    # draw scores
+    # Draw scores
     canvas.draw_text(str(score1), (200, 60), 40, 'Yellow')
     canvas.draw_text(str(score2), (400, 60), 40, 'Yellow')
         
@@ -125,11 +127,11 @@ def keyup(key):
     elif key==simplegui.KEY_MAP["down"]:
         paddle2_vel = 0
 
-# button to restart game
+# Button to restart game
 def button_handler():
     new_game()
 
-# create frame
+# Create frame
 frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
@@ -137,6 +139,6 @@ frame.set_keyup_handler(keyup)
 frame.add_button('Reset', button_handler, 50)
 
 
-# start frame
+# Start frame
 new_game()
 frame.start()
