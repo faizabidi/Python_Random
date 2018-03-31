ec0f6dda65de46975131e7f2b27807f1e465b82f
faizabidi <fabidi89@vt.edu>
2018-03-31 12:53:18 -0400

    4 weeks of the first course.

---
+++ DeepLearning-Coursera/Course1/week1/loss-fx.py
@@ -0,0 +1,40 @@
+import numpy as np 
+
+# http://rishy.github.io/ml/2015/07/28/l1-vs-l2-loss/
+
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+    
+    loss = np.sum(abs(yhat - y))
+     
+    return loss
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L1 = " + str(L1(yhat,y)))
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+    
+    loss = np.dot(y-yhat, y-yhat)
+    
+    return loss
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L2 = " + str(L2(yhat,y)))
