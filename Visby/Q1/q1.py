2431ff1f91855029ec69f56cc95b0f07bfcedf6d
faizabidi <fabidi89@vt.edu>
2018-06-19 16:58:42 -0400

    Work in progress.

--- Visby/Q1/q1.py
+++ Visby/Q1/q1.py
@@ -1,8 +1,9 @@
 import numpy as np
+import os
 
 x,y,z = raw_input("Please enter x, y, and z angles in degrees using commas. For example - \"45,60,90\"\n").split(',')
 
-# Convert to radion
+# Convert to radians
 x = np.radians(int(x))
 y = np.radians(int(y))
 z = np.radians(int(z))
@@ -31,23 +32,30 @@ file = open("tc1.bin", "r")
 dt = np.dtype(int)
 array = np.fromfile(file, dtype='uint32')
 
-# Store in a list of lists
+# Remove the results file if alreafdy exists
+myFile = "tc1-results.txt"
+if os.path.isfile(myFile):
+	os.remove(myFile)
+
+with open("tc1-results.txt", "a") as file:
+	file.write(str(array[0]) + '\n')
+
 i = 1
-j = 0
-while i+3 < len(array):
+index = 0
+while i+3 <= len(array):
+	# Take the first three tuples
 	values = array[i:i+3]
 	# Rotate these values using the rotation matrix
 	values = np.dot(values, rotation_matrix)
+	# Convert back to a list
 	values = np.array(values)[0].tolist()
-	#print(values[0])
-	#x,y,y = values[0], values[1], values[2]
-	# Convert it back to a list
-	#list(np.array(values.reshape(-1,)))
-	#values = ",".join(values).replace(",", " ")
+	# Prepend the index value
+	values =  str(index) + ' ' + ' '.join(map(str, values))
+	# Add to a file
+	with open("tc1-results.txt", "a") as file:
+		file.write(values + '\n')
 	i += 3
-	j += 1
-	print (values)
-
+	index += 1
 
 # References:
 # 1. https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions#Euler_angles_.28_z-y.E2.80.99-x.E2.80.B3_intrinsic.29_.E2.86.92_Rotation_matrix
